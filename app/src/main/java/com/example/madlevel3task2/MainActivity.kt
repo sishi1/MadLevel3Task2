f8d9a98f2c92749a22a8c8fbc69ca246425da3eb
Sishi <sishi_hu12@hotmail.com>
2020-10-18 11:17:53 +0200

    Added mainactivity code

--- app/src/main/java/com/example/madlevel3task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel3task2/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.example.madlevel3task2
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.example.madlevel3task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        navController = findNavController(R.id.nav_host_fragment)
+
+        binding.fab.setOnClickListener {
+            navController.navigate(
+                R.id.action_overviewFragment_to_addPortalFragment2
+            )
+        }
+
+        fabToggler()
+    }
+
+    private fun fabToggler() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id in arrayOf(R.id.overviewFragment))
+                binding.fab.show()
+            else
+                binding.fab.hide()
         }
     }
 
